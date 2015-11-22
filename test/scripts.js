5945f054a74daaf26c62b323aa07422ea34a8e7c
David Bonnet <david@bonnet.cc>
2015-11-22 23:11:32 +0100

    Updated full test

--- test/scripts.js
+++ test/scripts.js
@@ -6,50 +6,64 @@ var acorn = require( 'acorn' )
 var astravel = require( 'astravel' )
 var astring
 try {
-  astring = require( '../dist/astring.debug' )
-  console.log( 'Using ./dist/astring.debug.js' )
+	astring = require( '../dist/astring.debug' )
+	console.log( 'Using ./dist/astring.debug.js' )
 } catch ( error ) {
-  astring = require( '../dist/astring.min' )
-  console.log( 'Using ./dist/astring.min.js' )
+	astring = require( '../dist/astring.min' )
+	console.log( 'Using ./dist/astring.min.js' )
 }
 
 
-describe( 'Scripts tree comparison', function() {
-  this.timeout(0)
-  var pattern = path.join( __dirname, '../node_modules/{watchify,uglify-js,astravel,escodegen,babelify,acorn,minifyify,mocha,esotope,browserify,glob,benchmark}/**/*.js' )
-  var options = {
-    ecmaVersion: 6,
-    sourceType: 'module',
-    allowHashBang: true
-  }
+var dependencies = Object.getOwnPropertyNames(
+	require( '../package.json' ).devDependencies
+)
 
-  var stripLocation = astravel.makeTraveler( {
-     go: function( node, state ) {
-        delete node.start;
-        delete node.end;
-        this[node.type]( node, state );
-     }
-  } );
 
-  console.log( 'Looking for files…' )
-  var files = glob.sync( pattern, {
-    nodir: true
-  } )
-  console.log( 'Found', files.length, 'files' )
-  var length = path.join( __dirname, '../node_modules/' ).length;
-  files.forEach( function( filename ) {
-    var code = fs.readFileSync( filename, 'utf8' )
-    try {
-      var ast = acorn.parse( code, options )
-      stripLocation.go( ast )
-      // console.log( JSON.stringify(ast, null, 2) )
-    } catch ( error ) {
-      return
-    }
-    it( filename.substr( length ), function() {
-      var formattedAst = acorn.parse( astring( ast ), options )
-      stripLocation.go( formattedAst )
-      assert.deepEqual( formattedAst, ast )
-    } )
-  } )
+var pattern = path.join(
+	__dirname,
+	'../node_modules/{' + dependencies.join( ',' ) + '}/**/*.js'
+)
+var options = {
+	ecmaVersion: 6,
+	sourceType: 'module',
+	allowHashBang: true
+}
+
+var stripLocation = astravel.makeTraveler( {
+	go: function( node, state ) {
+		delete node.start
+		delete node.end
+		this[ node.type ]( node, state )
+	}
 } )
+
+console.log( 'Looking for files…' )
+var files = glob.sync( pattern, {
+	nodir: true
+} )
+
+console.log( 'Found', files.length, 'files' )
+
+var processedFiles = 0, errorFiles = 0
+
+files.forEach( function( filename ) {
+	var code = fs.readFileSync( filename, 'utf8' )
+	try {
+		var ast = acorn.parse( code, options )
+		stripLocation.go( ast )
+		// console.log( JSON.stringify(ast, null, 2) )
+	} catch ( error ) {
+		return
+	}
+	try {
+		var formattedAst = acorn.parse( astring( ast ), options )
+		stripLocation.go( formattedAst )
+		assert.deepEqual( formattedAst, ast )
+	} catch ( errpr ) {
+		console.error( 'Error on file ', filename )
+		errorFiles++
+	}
+	processedFiles++
+} )
+
+console.log( 'Processed ' + processedFiles + ' files with ' + errorFiles + ' error' + ( errorsFiles === 1 ? '' : 's' ) )
