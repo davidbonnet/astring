5900dc5885588e8a6e6dea3c5fa2d1da4d906db9
Moti Zilberman <motiz88@gmail.com>
2016-08-06 01:30:57 +0300

    Allow omitting specifier in specifier under ExportDeclaration

--- src/astring.js
+++ src/astring.js
@@ -571,7 +571,7 @@ export const defaultGenerator = {
 					else {
 						let { name } = specifier.id
 						output.write( name )
-						if ( name !== specifier.name.name )
+						if ( specifier.name && name !== specifier.name.name )
 							output.write( ' as ' + specifier.name.name )
 					}
 					if ( ++i < length )
