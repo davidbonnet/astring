3aeed5740032772bc5b283ee095c73642b4cd49e
David Bonnet <david@bonnet.cc>
2016-11-01 16:20:19 +0100

    Updated exports tests to comply with ES 6 specs

--- test/syntax/export.js
+++ test/syntax/export.js
@@ -1,10 +1,9 @@
 export * from "module";
 export {name} from "module";
 export {a as b, c as d} from "module";
-export {a as b, c as d};
+export {b as a, d as c};
 export {};
 export default a = 42;
-export var a = 42;
-export let a = 42;
+export var e = 42;
+export let f = 42;
 export function test() {}
-export default function test() {}
