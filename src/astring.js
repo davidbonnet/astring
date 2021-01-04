7e482acfe7726e57fc9749fb806a15f79cec5303
David Bonnet <david@bonnet.cc>
2021-01-04 20:31:41 +0100

    chore: update coverage comments

--- src/astring.js
+++ src/astring.js
@@ -10,17 +10,17 @@
 
 const { stringify } = JSON
 
-/* istanbul ignore if */
+/* c8 ignore if */
 if (!String.prototype.repeat) {
-  /* istanbul ignore next */
+  /* c8 ignore next */
   throw new Error(
     'String.prototype.repeat is undefined, see https://github.com/davidbonnet/astring#installation',
   )
 }
 
-/* istanbul ignore if */
+/* c8 ignore if */
 if (!String.prototype.endsWith) {
-  /* istanbul ignore next */
+  /* c8 ignore next */
   throw new Error(
     'String.prototype.endsWith is undefined, see https://github.com/davidbonnet/astring#installation',
   )
