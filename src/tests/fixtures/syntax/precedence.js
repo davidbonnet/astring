ba090fdfbe1fe7d052388328814766c9b7110b05
Joe d'Elia <joe@delia.is>
2021-10-01 13:49:12 +0100

    Fixes Logical expressions and coalesce expressions cannot be mixed

--- src/tests/fixtures/syntax/precedence.js
+++ src/tests/fixtures/syntax/precedence.js
@@ -8,6 +8,7 @@ f = "a" + (1 + 2) + "b";
 g = "a" + (1 - 2) + "b";
 a = true || false && null;
 b = c == d || e != f;
+b = (c || d) ?? e;
 c = x instanceof y || x instanceof z;
 d = x == y && y != z;
 a = !false;
