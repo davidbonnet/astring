5032a3168fea61092ba552e0d5cbe51c15bdeceb
David Bonnet <david@bonnet.cc>
2021-11-21 17:29:03 +0100

    migrate to zora

--- src/tests/performance.js
+++ src/tests/performance.js
@@ -5,23 +5,26 @@ import test from 'ava'
 import { benchmark } from './benchmark'
 import { readFile } from './tools'
 
-const FIXTURES_FOLDER = path.join(__dirname, 'fixtures')
+const FIXTURES_FOLDER = path.join(
+  path.dirname(new URL(import.meta.url).pathname),
+  'fixtures',
+)
 
 test('Performance tiny code', (assert) => {
   const result = benchmark('var a = 2;', 'tiny code')
-  assert.true(
+  assert.ok(
     result['astring'].speed > result['escodegen'].speed,
     'astring is faster than escodegen',
   )
-  assert.true(
+  assert.ok(
     result['astring'].speed > 10 * result['babel'].speed,
     'astring is at least 10x faster than babel',
   )
-  assert.true(
+  assert.ok(
     result['astring'].speed > 10 * result['prettier'].speed,
     'astring is at least 10x faster than prettier',
   )
-  assert.true(
+  assert.ok(
     result['acorn + astring'].speed > result['buble'].speed,
     'astring is faster than buble',
   )
@@ -32,19 +35,19 @@ test('Performance with everything', (assert) => {
     readFile(path.join(FIXTURES_FOLDER, 'tree', 'es6.js')),
     'everything',
   )
-  assert.true(
+  assert.ok(
     result['astring'].speed > result['escodegen'].speed,
     'astring is faster than escodegen',
   )
-  assert.true(
+  assert.ok(
     result['astring'].speed > 10 * result['babel'].speed,
     'astring is at least 10x faster than babel',
   )
-  assert.true(
+  assert.ok(
     result['astring'].speed > 10 * result['prettier'].speed,
     'astring is at least 10x faster than prettier',
   )
-  assert.true(
+  assert.ok(
     result['acorn + astring'].speed > result['buble'].speed,
     'astring is faster than buble',
   )
